6cc27434c09878dd49d7d9836a0055c463051792
kidlatpogi <dzeref4000@gmail.com>
2025-11-24 21:08:48 +0800

    chore: prepare branch 2.2.2

--- start_linny.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM L.I.N.N.Y. 3.0 - Headless Startup Script
-REM This script starts L.I.N.N.Y. in headless mode with fake lock feature
-
-REM Hide this CMD window
-if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
-
-cd /d "d:\Codes\Python\Linny"
-start /min pythonw.exe linny_app.py --startup
